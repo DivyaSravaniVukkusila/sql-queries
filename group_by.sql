2bc9ca4b8c8f7af0ef09ac9c77388221bac85c7c
John Davison <johncdavison@gmail.com>
2014-08-25 16:55:28 -0700

    Finishing stuff.

--- group_by.sql
+++ group_by.sql
@@ -54,27 +54,48 @@ ORDER BY year ASC, month ASC;
 -- A list of the top 5 US states by number of invoices
 -- Hint: You'll need to filter the results with WHERE billing_country = 'USA'
 
+SELECT billing_state, count(*) FROM INVOICES WHERE billing_country IS "USA" GROUP BY billing_state ORDER BY COUNT(*) DESC LIMIT 5;
+
 -- A list of the top 5 US states by gross sales
 
+SELECT billing_state, SUM(TOTAL) as total_sales FROM INVOICES WHERE billing_country IS "USA" GROUP BY billing_state ORDER BY total_sales DESC LIMIT 5;
+-- http://cl.ly/image/0f2D1I2J0D0i
+
 -- A list of the top 5 US states by average invoice size
+SELECT billing_state, AVG(TOTAL) as average_sales FROM INVOICES WHERE billing_country IS "USA" GROUP BY billing_state ORDER BY average_sales DESC LIMIT 5; 
+-- http://cl.ly/image/1c3b0h2l282Q
 
 -- A list of the top 10 US cities by number of invoices
+SELECT billing_state, COUNT(*) as qty_invoices FROM INVOICES WHERE billing_country IS "USA" GROUP BY billing_state ORDER BY qty_invoices DESC LIMIT 10;
+-- http://cl.ly/image/2t0M2P3c0V2S
 
 -- A list of the top 10 US cities by gross sales
+SELECT billing_city, SUM(total) AS total_sales FROM invoices WHERE billing_country IS "USA" GROUP BY billing_city ORDER BY total_sales DESC LIMIT 10 ;
+-- http://cl.ly/image/1k0s3t0b071T
 
 -- A list of the top 10 US cities by average invoice size
-
+SELECT billing_city, AVG(total) AS average_sales FROM invoices WHERE billing_country IS "USA" GROUP BY billing_city ORDER BY average_sales DESC LIMIT 10 ;
+-- http://cl.ly/image/3h2h3b41081i
 -- A list of the top 3 cities in California by number of invoices
 -- Hint: You'll need a WHERE clause filtering by both billing_country and billing_state
 
+select * from invoices where billing_state LIKE "ca"
+-- this is wierd query given this data
+
 -- A list of the top 3 cities in California by gross sales
+SELECT billing_city, SUM(total) AS gross_sales FROM invoices WHERE billing_country IS "USA" AND billing_state IS "CA" GROUP BY billing_city ORDER BY gross_sales;
+-- http://cl.ly/image/3c1S0V0r1H2H
 
 -- A list of the top 3 cities in California by average invoice size
-
+SELECT billing_city, AVG(total) AS avg_sales FROM invoices WHERE billing_country IS "USA" AND billing_state IS "CA" GROUP BY billing_city ORDER BY avg_sales;
+-- http://cl.ly/image/0x1E0Y1h052b
 
 -- "customers" table
 -- Remember: run ".schema customers" to see what fields (columns) the customers table contains.
 
 -- A list of the top 3 countries by total number of customers
-
+SELECT country, COUNT(*) FROM customers GROUP BY country ORDER BY COUNT(*) DESC LIMIT 3 ;
+-- http://cl.ly/image/190Q3y2v1g15
 -- A list of the top 7 cities (anywhere) by total number of customers
+SELECT city , COUNT(*) as qty_customers FROM CUSTOMERS GROUP BY CITY ORDER BY qty_customers DESC LIMIT 7 ;
+-- http://cl.ly/image/3D3Z0y183S2B
--- queries.sql
+++ queries.sql
@@ -36,39 +36,47 @@ SELECT * FROM albums WHERE title LIKE 'A%' ORDER BY title;
 -- Remember: run ".schema invoices" to see the structure of the "invoices" table
 
 -- Every invoice
+SELECT * FROM invoices;
 
 -- Every invoice ordered by total invoice amount ("total")
+SELECT * FROM invoices ORDER BY total;
 
 -- Every invoice with a total greater than 10
+SELECT * FROM invoices WHERE total > 10;
 
 -- The 10 least expensive invoices
 -- Remember: ORDER BY orders from lowest-to-highest by default
 SELECT * FROM invoices ORDER BY total LIMIT 10;
+SELECT * FROM invoices ORDER BY total ASC LIMIT 10;
 
 -- The 10 most expensive invoices
+SELECT * FROM invoices ORDER BY total DESC LIMIT 10;
 
 -- The 15 most recent invoices
+SELECT * FROM invoices ORDER BY invoice_date DESC LIMIT 15;
 
 -- The 15 oldest invoices
+SELECT * FROM invoices ORDER BY invoice_date ASC LIMIT 15;
 
 -- The 10 most expensive invoices from the US
-
+SELECT * FROM invoices WHERE billing_country IS "USA" ORDER BY total DESC LIMIT 10;
 -- The 10 least expensive invoices from the US
+SELECT * FROM invoices WHERE billing_country IS "USA" ORDER BY total ASC LIMIT 10;
 
 -- The 10 most expensive invoices from outside the US
 -- Hint: If "=" means equal, use "!=" to mean "not equal"
+SELECT * FROM invoices WHERE billing_country IS NOT "USA" ORDER BY total DESC LIMIT 10;
 
 -- Every invoice from Chicago, IL
 SELECT * FROM invoices WHERE billing_city = "Chicago" AND billing_state = "IL" AND billing_country="USA";
 
 -- A list of all the invoices worth more than $5.00 from Chicago, IL
+SELECT * FROM invoices WHERE billing_city IS "Chicago" AND total > "5";
 
--- The billing addresses of the 5 most valuable invoices from Mountain View CA
--- Gotta reward those big spenders!
-
+SELECT billing_address, total FROM invoices WHERE billing_city IS "Mountain View" ORDER BY total DESC limit 5;
 -- A list of the 10 most valuable invoices made before January 1st, 2010
 -- Hint: Dates are formatted like 'YYYY-MM-DD' and you can compare them using '<', '>', '<=' and '>='
-
+SELECT * FROM INVOICES WHERE invoice_date < "2010-01-01" ORDER BY total DESC LIMIT 10;
 
 -- The number of invoices from Chicago, IL
 SELECT COUNT(*) FROM invoices WHERE billing_city = "Chicago" AND billing_state = "IL" AND billing_country = "USA";
@@ -90,8 +98,18 @@ SELECT billing_state, COUNT(*), SUM(total), AVG(total) FROM invoices WHERE billi
 
 -- The count, total, and average of invoice totals, grouped by state, ordered by average invoice total from highest-to-lowest
 
+SELECT billing_state, COUNT(*), SUM(total), AVG(total) FROM INVOICES GROUP BY billing_state ORDER BY AVG(total) DESC ;
+
+SELECT billing_state, COUNT(*), SUM(total), AVG(total) FROM INVOICES GROUP BY billing_state ORDER BY billing_state DESC ;
+
 -- A list of the top 5 countries by number of invoices
+
+SELECT billing_country, COUNT(*) FROM INVOICES GROUP BY billing_country ORDER BY COUNT(*) DESC  LIMIT 5 ;
 
 -- A list of the top 5 countries by gross/total invoice size
 
+SELECT billing_country, SUM(total) FROM INVOICES GROUP BY billing_country ORDER BY SUM(TOTAl) DESC LIMIT 5 ;
+
 -- A list of the top 5 countries by average invoice size
+
+SELECT billing_country, AVG(total) FROM INVOICES GROUP BY billing_country ORDER BY AVG(TOTAl) DESC LIMIT 5 ;
